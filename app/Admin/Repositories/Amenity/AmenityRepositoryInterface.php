46a30f5b937f609d996a1d136a95061ca26a2bf8
minhttran <trcongminh.261002@gmail.com>
2025-01-14 12:38:35 +0700

    Add amenity management: implement AmenityRepository, AmenityRepositoryInterface, AmenityService, and AmenityServiceInterface; add store and update methods with default image handling

---
+++ app/Admin/Repositories/Amenity/AmenityRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Admin\Repositories\Amenity;
+
+use App\Admin\Repositories\BaseRepositoryInterface;
+
+interface AmenityRepositoryInterface extends BaseRepositoryInterface
+{
+    //
+}
